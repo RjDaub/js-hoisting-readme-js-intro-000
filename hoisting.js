18c5f28c8bb75594616dfa59c63c4ca9e17a8c71
Robert Daub <rjd2066@gmail.com>
2020-04-05 04:59:19 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -3,9 +3,11 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
-  return lyric;
   var lyric = "maybe";
+  return lyric;
 }
+callMe();
+console.log(lyric);
 
 
 function crazy() {
